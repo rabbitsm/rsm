ea537490aa2ddcc2e4d239e07ab412ab9a4f731e
maddhruv <dhruvjainpenny@gmail.com>
2018-06-17 13:06:56 +0530

    publish author details as well

--- cli/commands/publish.js
+++ cli/commands/publish.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 const request = require('superagent')
+const authorer = require('authorer')
 
 module.exports = function() {
   if(fs.existsSync('rsm.json')) {
     fs.readFile('./rsm.json', (err, data) => {
       if (err) throw err
       var rsmData = JSON.parse(data)
+      if(rsmData.author) rsmData.author = authorer(rsmData.author)
       if(!rsmData.name) {
         console.error('name is required')
         process.exit(0)
@@ -21,7 +23,7 @@ module.exports = function() {
         .send(rsmData)
         .set('accept', 'json')
         .end((err, res) => {
-          if (res.status == 409) {
+          if (res.status == 400) {
             console.error(`${rsmData.name} with version ${rsmData.version} already exists in the registry`)
             process.exit(0)
           }
