e95fed005eae1a986088d09aaac7574a7f743f7f
maddhruv <dhruvjainpenny@gmail.com>
2018-06-20 01:08:05 +0530

    added description to commands

--- index.js
+++ index.js
@@ -11,27 +11,32 @@ program
 
 program
   .command('init')
+  .description('initializes a rsm image')
   .action(init)
 
 program
   .command('publish')
   .option('-c, --config <file>', 'custom config file')
+  .description('publish your local image to the rsm registry')
   .action(publish)
 
 program
   .command('setup')
   .alias('s')
+  .description('setup your local machine to be able to install')
   .action(setup)
 
 program
   .command('install <name> [version]')
   .alias('i')
+  .description('install rsm image (software/package/language)')
   .action(install)
 
 program
   .command('find <name>')
   .alias('f')
   .option('-l --list', 'list all the available versions')
+  .description('finds any rsm image')
   .action(find)
 
 program.parse(process.argv)
